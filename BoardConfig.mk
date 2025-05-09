32f3a39ed6527ccaec04ef99c5bd83dac5a924f4
zenin1504 <kayozzprojects@yandex.com>
2025-05-09 19:57:47 +0000

    [SQUASH] duchamp: Initial micam for duchamp
    * also import patches for micam
    
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>
    
    duchamp: sepolicy: Label more property
    
    libc    : Access denied finding property ro.camera.req.fmq.size
    libc    : Access denied finding property ro.camera.res.fmq.size
    libc    : Access denied finding property ro.vendor.audio.zoom.type
    libc    : Access denied finding property ro.miui.region
    libc    : Access denied finding property ro.miui.build.region
    
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>
    
    duchamp: sepolicy: fix selinux errors 6adc1b6
    
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+MIUICAMERA_PATH := device/xiaomi/duchamp-miuicamera
+
+# Properties
+TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
+
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/private
+
+# Inherit from the proprietary version
+include vendor/xiaomi/duchamp-miuicamera/BoardConfigVendor.mk
