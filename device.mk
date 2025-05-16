c6e4cf1d82169e2cd977c861729a3f323803fd11
zenin1504 <kayozzprojects@yandex.com>
2025-05-16 21:47:42 +0000

    Revert "duchamp: Import Poco X6 Pro 5G Device Features"
    This reverts commit 035d26cf67695608997f2e8a8314d1ea268c52d2.

--- device.mk
+++ device.mk
@@ -16,10 +16,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml
 
-# Device-Features
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/device_features/duchamp.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/duchamp.xml
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
