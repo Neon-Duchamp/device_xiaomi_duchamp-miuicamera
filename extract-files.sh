0d4f1cd43176e2cf2380adc99efd3b3b908ef2e7
zenin1504 <kayozzprojects@yandex.com>
2025-07-01 17:12:26 +0000

    duchamp: Drop apk patching and python replace with makefile
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

---
+++ extract-files.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=duchamp-miuicamera
+VENDOR=xiaomi
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+KANG=
+SECTION=
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
+    esac
+    shift
+done
+
+if [ -z "${SRC}" ]; then
+    SRC="adb"
+fi
+
+function blob_fixup() {
+    case "${1}" in
+        system_ext/priv-app/MiuiCamera/MiuiCamera.apk)
+            # Replace Miui Gallery with Google Photos
+            tmp_dir="${EXTRACT_TMP_DIR}/MiuiCamera"
+            apktool d -q "$2" -o "$tmp_dir" -f
+            grep -rl "com.miui.gallery" "$tmp_dir" | xargs sed -i 's|com\.miui\.gallery|com\.google\.android\.apps\.photos|g'
+            apktool b -q "$tmp_dir" -o "$2"
+            rm -rf "$tmp_dir"
+            ;;
+        system_ext/lib64/libcamera_algoup_jni.xiaomi.so|\
+        system_ext/lib64/libcamera_mianode_jni.xiaomi.so|\
+        system_ext/lib64/libcamera_ispinterface_jni.xiaomi.so)
+            "${PATCHELF}" --add-needed "libgui_shim_miuicamera.so" "${2}"
+            ;;
+        system_ext/lib64/vendor.mediatek.hardware.camera.isphal@1.0.so)
+            "${PATCHELF}" --replace-needed "android.hardware.graphics.common-V1-ndk.so" \
+                "android.hardware.graphics.common-V2-ndk.so" "${2}"
+            ;;
+    esac
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+"${MY_DIR}/setup-makefiles.sh"
