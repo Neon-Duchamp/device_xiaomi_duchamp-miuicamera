Neon-Duchamp/device_xiaomi_duchamp

duchamp: Enable CameraX extensions support
- Set `ro.camerax.extensions.enabled=true` in both system.prop and odm.prop
- Add `camerax-vendor-extensions.jar` to system_ext
- Grant required permission via `camerax-vendor-extensions.xml`

Enables support for enhanced camera features via CameraX Extensions, improving compatibility with modern camera apps.

Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

device.mk
@@ -107,6 +107,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.example
 
+# CameraExtensions
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/permissions/camerax-vendor-extensions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/camerax-vendor-extensions.xml
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.memtrack-service.mediatek-mali

Neon-Duchamp/device_xiaomi_duchamp-miuicamera

device.mk
@@ -20,5 +20,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# CameraExtensions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/camerax-vendor-extensions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/camerax-vendor-extensions.xml
+
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/duchamp-miuicamera/duchamp-miuicamera-vendor.mk)